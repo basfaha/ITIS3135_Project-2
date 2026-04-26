344b6f38bbaee0beaf2b85e63fcee4b4ce68c144
Beyene Asfaha <117701730+basfaha@users.noreply.github.com>
2026-04-26 14:12:35 -0400

    no change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import BlogPage from './pages/BlogPage';
+import ProfilePage from './pages/ProfilePage';
 import './App.css';
 
 export default function App() {
@@ -13,13 +14,19 @@ export default function App() {
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/blog"
-            element={<BlogPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
       </BrowserRouter>
     </AuthProvider>
   );
